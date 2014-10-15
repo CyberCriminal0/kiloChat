49f968f0dd4946528284122973b12cf50e7c6ce3
Jack Eller <Jack@192.168.1.8>
2014-10-15 00:02:35 -0400

    Login now contains basic fields

--- kiloChat/src/kiloChat/login.java
+++ kiloChat/src/kiloChat/login.java
@@ -7,10 +7,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtAddress;
+	private JTextField textPort;
 
 	/**
 	 * Launch the application.
@@ -32,6 +39,14 @@ public class login extends JFrame {
 	 * Create the frame.
 	 */
 	public login() {
+		/**try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		**/
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -40,6 +55,35 @@ public class login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setBounds(131, 45, 56, 14);
+		contentPane.add(lblName);
+		
+		txtName = new JTextField();
+		txtName.setText("Name");
+		txtName.setBounds(80, 70, 134, 30);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(111, 129, 82, 30);
+		contentPane.add(lblIpAddress);
+		
+		textPort = new JTextField();
+		textPort.setText("Address");
+		textPort.setColumns(10);
+		textPort.setBounds(80, 170, 134, 30);
+		contentPane.add(textPort);
+		
+		JLabel lblPort = new JLabel("Port:");
+		lblPort.setBounds(131, 226, 31, 30);
+		contentPane.add(lblPort);
+		
+		txtAddress = new JTextField();
+		txtAddress.setText("Port");
+		txtAddress.setColumns(10);
+		txtAddress.setBounds(80, 267, 134, 30);
+		contentPane.add(txtAddress);
 	}
-
 }
