9e1c64d85672bfa47a68e754e52085b17a30cc12
Jack Eller <Jack@192.168.1.8>
2014-10-14 23:36:24 -0400

    Added blank Login frame

--- kiloChat/src/kiloChat/login.java
+++ kiloChat/src/kiloChat/login.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JProgressBar;
 
 public class login extends JFrame {
 
@@ -31,12 +32,14 @@ public class login extends JFrame {
 	 * Create the frame.
 	 */
 	public login() {
+		setResizable(false);
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 300, 400);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
